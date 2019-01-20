1450ac56cdab4f7890b1ffebae6b02f1d53ba019
Cheddi Charles <cheddi.charles@gmail.com>
2019-01-19 19:14:17 -0600

    Updates

---
+++ precache-manifest.cd9f5ec6f5fc22808f1e2c900de20f7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94e7fc0bf6f2e59c4e06",
+    "url": "/dc-memory-click/static/js/runtime~main.94e7fc0b.js"
+  },
+  {
+    "revision": "c9bcbea6b2f6b0cd3fb9",
+    "url": "/dc-memory-click/static/js/main.c9bcbea6.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/dc-memory-click/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "c9bcbea6b2f6b0cd3fb9",
+    "url": "/dc-memory-click/static/css/main.20684764.chunk.css"
+  },
+  {
+    "revision": "65735b00234475e556e50b831277d131",
+    "url": "/dc-memory-click/index.html"
+  }
+];
